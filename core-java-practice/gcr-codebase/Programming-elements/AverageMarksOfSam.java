9eafd6490f5189530b376c5179eff2ea8e2592d3
SubhashSingh01 <sobbythakur8805@gmail.com>
2025-12-18 11:21:10 +0530

    first program

---
+++ core-java-practice/gcr-codebase/Programming-elements/AverageMarksOfSam.java
@@ -0,0 +1,10 @@
+public class AverageMarksOfSam {
+    public static void main(String[] args) {
+        int maths = 85; // Marks in maths
+        int physics= 90; // Marks in physics
+        int chemistry = 78; // Marks in chemistry
+        int totalMarks = maths + physics + chemistry; // Calculate total marks
+        double averageMarks = totalMarks / 3.0; // Calculate average marks
+        System.out.println("Sam's average marks are: " + averageMarks);
+    }
+}
---
+++ core-java-practice/gcr-codebase/Programming-elements/DistanceConvert.java
@@ -0,0 +1,7 @@
+public class DistanceConvert {
+    public static void main(String[] args) {
+        double kilometers = 10.8; // Distance in kilometers
+        double miles = kilometers * 0.621371; // Convert to miles
+        System.out.println("The distance "+ kilometers + "km in miles is " + miles);
+    }
+}
